edb048795d9a5dd04e5805bbeb5ff5f20ea068cd
NithinManmohan <nithin.infinite@gmail.com>
2014-01-15 13:46:58 +0530

    Update PDFQubePostJob.java

--- src/main/java/com/pdfqube/postjob/PDFQubePostJob.java
+++ src/main/java/com/pdfqube/postjob/PDFQubePostJob.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.util.Date;
 import java.util.List;
 
+import org.jfree.chart.block.CenterArrangement;
 import org.sonar.api.batch.PostJob;
 import org.sonar.api.batch.SensorContext;
 import org.sonar.api.resources.Project;
@@ -17,16 +18,52 @@ import org.sonar.wsclient.services.ResourceQuery;
 import org.sonar.wsclient.services.Violation;
 import org.sonar.wsclient.services.ViolationQuery;
 
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class PDFQubePostJob implements PostJob {
+
+	private static void addTitlePage(Document document)
+			throws DocumentException {
+		Font fontbold = FontFactory.getFont("Times-Roman", 36, Font.BOLD);
+
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Phrase("\n"));
+		document.add(new Paragraph("Sonar PDF Report", fontbold));
+		document.add(new Paragraph("Created by PDF Qube Plugin"));
+
+	}
 
 	public void executeOn(Project project, SensorContext arg1) {
 		// TODO Auto-generated method stub
 
 		try {
+			String fileName = "SonarReport.pdf";
+
+			Font fontHeading = FontFactory
+					.getFont("Times-Roman", 24, Font.BOLD);
+
+			File file = new File(fileName);
+			if (file.exists()) {
+				file.delete();
+			}
+
 			String url = "http://localhost:9000";
 			String login = "admin";
 			String password = "admin";
@@ -35,13 +72,14 @@ public class PDFQubePostJob implements PostJob {
 			Resource struts = sonar.find(ResourceQuery.createForMetrics(
 					project.getKey(), "coverage", "lines", "violations",
 					"duplicated_lines", "ncloc", "packages", "classes",
-					"functions"));
+					"functions", "sqale_index", "files", "tests",
+					"test_execution_time", "test_errors", "test_failures",
+					"test_success_density"));
 
-			String fileName = project.getName() + ".pdf";
-			OutputStream file = new FileOutputStream(new File(fileName));
+			OutputStream report = new FileOutputStream(new File(fileName));
 
 			Document document = new Document();
-			PdfWriter.getInstance(document, file);
+			PdfWriter.getInstance(document, report);
 
 			String lines = struts.getMeasure("lines").getIntValue().toString();
 			String codelines = struts.getMeasure("ncloc").getValue().toString();
@@ -53,15 +91,23 @@ public class PDFQubePostJob implements PostJob {
 					.toString();
 			String noofviolations = struts.getMeasure("violations").getValue()
 					.toString();
+			String techDebt = struts.getMeasure("sqale_index")
+					.getFormattedValue().toString();
+			String noofFiles = struts.getMeasure("files").getValue().toString();
 
 			document.open();
-			document.add(new Paragraph("Sonar PDF Report using PDF Qube Plugin"));
-			document.add(new Paragraph(new Date().toString()));
+			/*			*/
+			addTitlePage(document);
 
 			// Page 1
 
 			document.newPage();
-			document.add(new Paragraph("Total Number of Lines of Code:" + lines));
+			document.add(new Phrase("\n"));
+			document.add(new Phrase("\n"));
+			document.add(new Paragraph("Overview details", fontHeading));
+			document.add(new Phrase("\n"));
+
+			document.add(new Paragraph("Total Number of Lines:" + lines));
 			document.add(new Paragraph("Total Number of Lines of Code: "
 					+ codelines));
 			document.add(new Paragraph("Total Number of Packages: "
@@ -72,9 +118,12 @@ public class PDFQubePostJob implements PostJob {
 					+ nooffunctions));
 			document.add(new Paragraph("Total Number of Violations: "
 					+ noofviolations));
+			document.add(new Paragraph("Technical Debt: " + techDebt));
+			document.add(new Paragraph("Total Number of Files: " + noofFiles));
 
 			document.newPage();
-			document.add(new Paragraph("Violations: "));
+			document.add(new Paragraph("Violations", fontHeading));
+			document.add(new Phrase("\n"));
 
 			ViolationQuery violationQuery = ViolationQuery
 					.createForResource(project.getKey());
@@ -91,7 +140,7 @@ public class PDFQubePostJob implements PostJob {
 			}
 
 			document.close();
-			file.close();
+			report.close();
 
 		} catch (Exception e) {
 
